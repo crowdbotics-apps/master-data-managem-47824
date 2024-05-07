e71ab50f942fd247d8e056187884e3a38daa7bcc
Crowdbotics <team@crowdbotics.com>
2024-05-07 02:52:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "master-data-managem-47824",
+  url: "https://master-data-managem-47824.botics.co",
+  api: "https://master-data-managem-47824.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
